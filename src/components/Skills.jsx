b9c1fce52bcdac784bd4ea27ab5ab0731688b671
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-04-03 17:47:49 +0200

    skill fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-import HTML from "../assets/html.png";
-import CSS from "../assets/css.png";
-import JavaScript from "../assets/javascript.png";
-import ReactImg from "../assets/react.png";
-import Bootstrap from "../assets/Bootstrap.png";
-import Google from "../assets/Google.png";
-import GitHub from "../assets/github.png";
-import Tailwind from "../assets/tailwind.png";
-
 const Skills = () => {
 	return (
 		<div
@@ -25,36 +16,60 @@ const Skills = () => {
 				</div>
 
 				<div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon html-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon html-icon"></div>
+							<div class="skill-icon html-icon"></div>
+						</div>
 						<p className="my-4">HTML</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon css-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon css-icon"></div>
+							<div class="skill-icon css-icon"></div>
+						</div>
 						<p className="my-4">CSS</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon javascript-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon javascript-icon"></div>
+							<div class="skill-icon javascript-icon"></div>
+						</div>
 						<p className="my-4">JavaScript</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon react-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon react-icon"></div>
+							<div class="skill-icon react-icon"></div>
+						</div>
 						<p className="my-4">React</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon bootstrap-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon bootstrap-icon"></div>
+							<div class="skill-icon bootstrap-icon"></div>
+						</div>
 						<p className="my-4">Bootstrap</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-					<div className="w-20 h-20 skill-icon tailwind-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon tailwind-icon"></div>
+							<div class="skill-icon tailwind-icon"></div>
+						</div>
 						<p className="my-4">Tailwind</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-						<div className="w-20 h-20 skill-icon google-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon google-icon"></div>
+							<div class="skill-icon google-icon"></div>
+						</div>
 						<p className="my-4">Google</p>
 					</div>
-					<div className="hover:scale-110 duration-500">
-						<div className="w-20 h-20 skill-icon github-icon"></div>
+					<div class="hover:scale-110 duration-500">
+						<div className="skill-icon-wrapper">
+							<div class="skill-icon github-icon"></div>
+							<div class="skill-icon github-icon"></div>
+						</div>
 						<p className="my-4">GitHub</p>
 					</div>
 				</div>
