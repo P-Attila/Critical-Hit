b88ed58c505f352e75ab997a9c03954091203660
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-04-03 11:17:29 +0200

    build fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -124,7 +124,8 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
 						<a
 							className="flex justify-between items-center w-full text-gray-300"
-							href="/"
+							href="../downloads/CV-Attila_Polgar.pdf"
+							target="_blank"
 						>
 							Resume <BsFillPersonLinesFill size={30} />
 						</a>
