e2233400215b28c05aeb6296309735f16bf0e161
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-04-26 20:12:17 +0200

    Change About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const About = () => {
 	return (
-		<div name="about" className="w-full flex items-center min-h-screen bg-zinc-900 text-gray-300">
+		<div
+			name="about"
+			className="w-full flex items-center min-h-screen bg-zinc-900 text-gray-300"
+		>
 			{/* Container */}
 			<div className="flex flex-col justify-center items-center w-full h-full">
 				<div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
@@ -15,15 +18,20 @@ const About = () => {
 				</div>
 				<div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
 					<div className="sm:text-right text-4xl font-bold">
-						<p>Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor.</p>
+						<p>
+							I'm a very self-conscious person who, for the sake of momentary
+							benefits, does not forget what he owes himself.
+						</p>
 					</div>
 					<div>
 						<p>
-							Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor.
-							Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor
-							hodor;Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor.
-							Hodor. Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor
-							hodor;
+							Blunt, big nose, weird mustache, and miserable face. Her health is
+							not entirely okay: she was injured, has high blood pressure, so
+							she should refrain from excessive physical exertion, and because
+							of her space, she likes to stay in closets. In the opinion of his
+							legionary superiors, he is “the pure breed of the spit citizen,”
+							an unfortunate spiritual figure, an anti-soldier, a primitive
+							civilian. He always has his own opinion on everything.
 						</p>
 					</div>
 				</div>
