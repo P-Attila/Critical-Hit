4e1d22b2317ae1d9bca0b6fc2ef6bdd12f209ded
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-05-02 10:34:36 +0200

    add demo shop

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,7 @@ import BuySomeRecords from "../assets/BuySomeRecords.png";
 import FlashCards from "../assets/Flash-Cards.png";
 import GrapplingCircle from "../assets/Grappling-circle.png";
 import MoviePoster from "../assets/Movie-Poster.png";
+import DemoShop from "../assets/demoShop.png";
 
 const Work = () => {
 	return (
@@ -151,6 +152,40 @@ const Work = () => {
 							</div>
 						</div>
 					</div>
+
+					{/* Grid Item */}
+					<div
+						style={{ backgroundImage: `url(${DemoShop})` }}
+						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+					>
+						{/* Hover Effects */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-xl font-bold text-white tracking-wider">
+							Next.js + Shopify + Builder.io
+							</span>
+							<div className="pt-8 text-center">
+								<a
+									href="https://demo-shop-rho.vercel.app/"
+									target="_blank"
+									rel="opener noreferrer"
+								>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+										Demo
+									</button>
+								</a>
+								<a
+									href="https://github.com/P-Attila/demo-shop"
+									target="_blank"
+									rel="opener noreferrer"
+								>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
